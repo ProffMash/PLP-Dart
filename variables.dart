250424d73cc29da834df1777d1a920ea9d01f6f8
david <kdavidmacharia8485@gmail.com>
2025-06-07 13:27:15 +0300

    variables

---
+++ variables.dart
@@ -0,0 +1,14 @@
+void main() {
+  //Variables
+  int age = 25;
+  double height = 5.9;
+  String name = "Alice";
+  bool isStudent = true;
+  List<int> grades = [90, 85, 92, 88];
+
+  print("Age: $age");
+  print("Height: $height");
+  print("Name: $name");
+  print("Is Student: $isStudent");
+  print("Grades: $grades");
+}
